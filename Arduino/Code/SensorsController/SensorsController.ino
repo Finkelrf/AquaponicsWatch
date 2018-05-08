e2690ad55b9b733957e2f95dfe123919d78babd7
rafael finkelstein <finkel.rf@gmail.com>
2018-05-08 08:10:19 -0300

    First commit

---
+++ Arduino/Code/SensorsController/SensorsController.ino
@@ -0,0 +1,166 @@
+#include <SoftwareSerial.h>
+#include <Ultrasonic.h>
+ 
+#define DEBUG
+#define VERSION "Version_0.1"
+#define MAX_STRING_LENGTH 50
+#define BEGIN_OF_COMMAND_CHAR '\r'
+#define SEPARATION_CHAR '|'
+#define END_OF_COMMAND_CHAR '\n'
+
+#define NUMBER_OF_SENSORS 1
+
+#define SENSOR_RAIN 0
+#define SENSOR_RAIN_TOPIC "fishtank/rain"
+#define SENSOR_RAIN_DEFAULT_INTERVAL 5000 //10 minutes
+#define SENSOR_RAIN_ANALOG_INPUT A0
+#define SENSOR_RAIN_POWER_PIN 6
+
+#define SENSOR_DISTANCE 1
+#define SENSOR_DISTANCE_TOPIC "fishtank/distance"
+#define SENSOR_DISTANCE_DEFAULT_INTERVAL 10000 //10 seconds
+#define SENSOR_DISTANCE_TRIGGER_PIN 6
+#define SENSOR_DISTANCE_ECHO_PIN 5
+#define DISTANCE_SENSOR_SAMPLES 100
+
+char msg[MAX_STRING_LENGTH];
+int i = 0;
+
+SoftwareSerial SoftSerial(10, 11); //RX, TX
+Ultrasonic ultrasonic(SENSOR_DISTANCE_TRIGGER_PIN, SENSOR_DISTANCE_ECHO_PIN);    
+
+struct sensorStruct {
+   unsigned long previousMillis;
+   unsigned long interval;
+   void (*execution)();
+};
+
+struct sensorStruct sensor[NUMBER_OF_SENSORS];
+
+void initSensors(){
+  //config rain sensor
+  sensor[SENSOR_RAIN].previousMillis = millis();
+  sensor[SENSOR_RAIN].interval = SENSOR_RAIN_DEFAULT_INTERVAL;
+  sensor[SENSOR_RAIN].execution = sendRainValue;
+  pinMode(SENSOR_RAIN_POWER_PIN,OUTPUT);
+  digitalWrite(SENSOR_RAIN_POWER_PIN,LOW);
+
+  //config distance sensor
+  pinMode(SENSOR_DISTANCE_TRIGGER_PIN, OUTPUT); // Sets the trigPin as an Output
+  pinMode(SENSOR_DISTANCE_ECHO_PIN, INPUT); // Sets the echoPin as an Input
+  sensor[SENSOR_DISTANCE].previousMillis = millis();
+  sensor[SENSOR_DISTANCE].interval = SENSOR_DISTANCE_DEFAULT_INTERVAL;
+  sensor[SENSOR_DISTANCE].execution = sendDistanceValue;
+}
+
+float sampleDistance(){
+  float measures[DISTANCE_SENSOR_SAMPLES];
+  for(int i=0;i<DISTANCE_SENSOR_SAMPLES;i++){
+    long microsec = ultrasonic.timing();
+    measures[i] = ultrasonic.convert(microsec, Ultrasonic::CM);
+  }
+  float total=0;
+  for(int i=0;i<DISTANCE_SENSOR_SAMPLES;i++){
+    total += measures[i];
+  }
+  total = total/DISTANCE_SENSOR_SAMPLES;
+  return total;
+}
+
+
+void sendRainValue(){
+  digitalWrite(SENSOR_RAIN_POWER_PIN,HIGH);
+  delay(500);
+  //SoftSerial.print(BEGIN_OF_COMMAND_CHAR);
+  SoftSerial.print(SENSOR_RAIN_TOPIC);
+  SoftSerial.print("|");
+  SoftSerial.println(1024-analogRead(SENSOR_RAIN_ANALOG_INPUT));
+  digitalWrite(SENSOR_RAIN_POWER_PIN,LOW);
+}
+
+void sendDistanceValue(){
+  //SoftSerial.print(BEGIN_OF_COMMAND_CHAR);
+  SoftSerial.print(SENSOR_DISTANCE_TOPIC);
+  SoftSerial.print("|");
+  float distance = sampleDistance();
+  SoftSerial.println(distance);
+  //#ifdef DEBUG
+    Serial.print("Distance: ");
+    Serial.println(distance);
+  //#endif
+}
+
+void setup() {
+  Serial.begin(9600);
+  SoftSerial.begin(9600);
+  Serial.println(VERSION);
+  initSensors();  
+}
+
+void parseMsg(String msg){
+  int commandEndIndex = msg.indexOf(SEPARATION_CHAR);
+  int deviceEndIndex = msg.indexOf(SEPARATION_CHAR,commandEndIndex+1);
+  int valueEndIndex = msg.indexOf(END_OF_COMMAND_CHAR,deviceEndIndex+1);
+  String command = msg.substring(0,commandEndIndex);
+  String device = msg.substring(commandEndIndex+1,deviceEndIndex);
+  String value = msg.substring(deviceEndIndex+1,valueEndIndex);
+
+  unsigned long *intervalPointer;
+  if(command.equals("interval")){
+    if(device.equals("rain")){
+      intervalPointer = &sensor[SENSOR_RAIN].interval;
+    }
+    if(device.equals("distance")){
+      intervalPointer = &sensor[SENSOR_DISTANCE].interval;
+    }
+    *intervalPointer = value.toInt();
+  }
+  #ifdef DEBUG
+    Serial.print("command: ");
+    Serial.println(command);
+    Serial.print("device: ");
+    Serial.println(device);
+    Serial.print("value: ");
+    Serial.println(value);
+    Serial.print("Rain interval: ");
+    Serial.println(sensor[SENSOR_RAIN].interval);
+    Serial.print("Distance interval: ");
+    Serial.println(sensor[SENSOR_DISTANCE].interval);
+  #endif
+}
+
+void readSerial(){
+  while (SoftSerial.available() > 0) {
+    char charRead = SoftSerial.read();
+    if(charRead == BEGIN_OF_COMMAND_CHAR){
+      i = 0;
+      memset(msg, 0, sizeof(msg));
+    }else if(charRead == END_OF_COMMAND_CHAR){
+      #ifdef DEBUG
+        Serial.print("Msg received: ");
+        Serial.println(msg);
+      #endif
+      parseMsg(String(msg));
+      i = 0;
+      memset(msg, 0, sizeof(msg));
+    }else{
+      msg[i] = charRead;
+      i++;
+    }
+  }
+}
+
+void loop() {
+  for(int i=0;i<NUMBER_OF_SENSORS;i++){
+    unsigned long currentMillis = millis();
+    if (currentMillis -  sensor[i].previousMillis >= sensor[i].interval) {
+       sensor[i].previousMillis = currentMillis;
+       sensor[i].execution();      
+    }
+  }
+
+  //readSerial();
+  if(SoftSerial.available()> 0){
+    Serial.print((char)SoftSerial.read());
+  }
+}
---
+++ ESP8266/Code/MqttRelay/MqttRelay.ino
@@ -0,0 +1,150 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <PubSubClient.h>
+
+//#define DEBUG
+#define VERSION "Version_0.1"
+
+const char* ssid = "MY_SSID";
+const char* password = "***********";
+const char* mqtt_server = "192.168.1.8";
+
+#define CONFIGURATION_TOPIC "sensors/config"
+#define MAX_STRING_LENGTH 50
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+char msg[MAX_STRING_LENGTH];
+int i = 0;
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  #ifdef DEBUG
+    Serial.println("Callback called");
+  #endif
+  Serial.print('\r');
+  for (int i = 0; i < length; i++) {
+    //forward messsage to arduino
+    Serial.print((char)payload[i]);
+  }
+  Serial.print('\n');
+}
+
+void reconnect() {
+  #ifdef DEBUG
+    Serial.println("Reconnect called");
+  #endif
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    // Attempt to connect
+    if (client.connect("ESP8266Client")) {
+      client.publish("logs","ESP8266 reconnected.");
+      client.subscribe(CONFIGURATION_TOPIC);
+    } else {
+      delay(5000);
+    }
+  }
+}
+
+void setupMqtt(){
+  #ifdef DEBUG
+    Serial.println("Setup mqtt called");
+  #endif
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+  if (!client.connected()) {
+    reconnect();
+  }
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  #ifdef DEBUG
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+  #endif
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    #ifdef DEBUG
+      Serial.println(WiFi.status());
+    #endif
+  }
+  #ifdef DEBUG
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  #endif
+  digitalWrite(2,LOW);
+}
+
+void setup() {
+  Serial.begin(9600);
+  #ifdef DEBUG
+    Serial.println(VERSION);
+  #endif
+  pinMode(2,OUTPUT);
+  digitalWrite(2,HIGH);
+  setup_wifi();
+  setupMqtt();
+  
+}
+
+void msgParser(char *msg, char *topic, char *payload){
+  #ifdef DEBUG
+    //Serial.println("Msg parser called");
+  #endif
+// message format => my/topic|My payload
+  int msgIndex = 0;
+  int topicIndex = 0;
+  int payloadIndex = 0;
+  char separator = '|';
+  while(msg[msgIndex]!=separator){
+    topic[topicIndex++] = msg[msgIndex++];
+  }
+  topic[topicIndex] = '\0';
+  msgIndex++;
+  while(msgIndex<MAX_STRING_LENGTH){
+    payload[payloadIndex++] = msg[msgIndex++];
+  }
+  payload[payloadIndex] = '\0';
+
+}
+
+void readSerial(){
+  while (Serial.available() > 0) {
+    char charRead = Serial.read();
+    if(charRead == '\n'){
+      #ifdef DEBUG
+        Serial.println("End of command reached");
+        Serial.println(msg);
+      #endif
+      i = 0;
+      char topic[MAX_STRING_LENGTH];
+      char payload[MAX_STRING_LENGTH];
+      msgParser(msg,topic,payload);
+      if (!client.connected()) {
+        reconnect();
+      }
+      client.publish(topic, payload);
+      memset(msg, 0, sizeof(msg));
+      memset(topic, 0, sizeof(topic));
+      memset(payload, 0, sizeof(payload));
+    }else{
+      msg[i] = charRead;
+      i++;
+    }
+  }
+}
+
+void loop() {    
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+  readSerial();
+}
