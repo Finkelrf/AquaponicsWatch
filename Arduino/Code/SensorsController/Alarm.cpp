904b3e53f376ca0d01c9798cd83cdb45d6a1e3f5
rafael finkelstein <finkel.rf@gmail.com>
2018-05-13 02:36:13 -0300

    Added start up delay for timers

--- Arduino/Code/SensorsController/Alarm.cpp
+++ Arduino/Code/SensorsController/Alarm.cpp
@@ -1,91 +1,115 @@
 #include "alarm.h"
 
+Timer_t Alarm::timers[] = {};
+int Alarm::alocatedTimers = 0;
+
 Alarm::Alarm() {
    int i;
    for (i = 0; i < MAX_TIMERS; ++i)
    {
-   	timers[i].running = false;
+   	Alarm::timers[i].running = false;
    }
 }
 
-void Alarm::repeatTimer(long interval, int repetitions, void (*callback)(void), bool addSalt){
-	long salt = 0;
-	if(addSalt){
-		randomSeed(analogRead(A5));
-		salt = random(interval/100);
-		Serial.print("salt: ");
-		Serial.println(salt);
-	}
-	Timer_t timer;
-	timer.lastTime = millis();
-	timer.interval = interval;
-	timer.repetitions = repetitions+salt;
-	timer.running = true;
-	timer.callback = callback;
-	addTimer(timer);
+void hello(int *i){
+	Serial.print("Hello ");
+	Serial.println(*i);
+}
 
+void Alarm::repeatTimer(long interval, int repetitions, void (*callback)(void *), void *data , bool addStartDelay, long startDelay){
+	Timer_t *timer = (Timer_t*)malloc(sizeof(Timer_t));
+	timer->lastTime = millis();
+	timer->interval = interval;
+	timer->repetitions = repetitions;
+	timer->running = true;
+	timer->callback = callback;
+	timer->data = data;
+
+	if(addStartDelay){
+#ifdef DEBUG
+		Serial.println("Start delay on!");
+#endif
+		if(startDelay == -1){
+			randomSeed(analogRead(A5));
+			startDelay = random(interval/100);
+#ifdef DEBUG
+			Serial.print("startDelay: ");
+			Serial.println(startDelay);
+#endif
+		}
+		oneTimeTimer(startDelay, Alarm::addTimer, timer);
+	}else{
+		addTimer(timer);
+	}
 };
 
-void Alarm::oneTimeTimer(long interval, void (*callback)(void)){
-	Timer_t timer;
-	timer.lastTime = millis();
-	timer.interval = interval;
-	timer.repetitions = 1;
-	timer.running = true;
-	timer.callback = callback;
-	addTimer(timer);
-
+void Alarm::infiniteTimer(long interval, void (*callback)(void *), void *data , bool addStartDelay, long startDelay){
+#ifdef DEBUG
+	Serial.println("Infinite timer scheduled.");
+#endif
+	repeatTimer(interval, -1,  callback, data, addStartDelay, startDelay);
 };
 
-void Alarm::infiniteTimer(long interval, void (*callback)(void), bool addSalt){
-	long salt = 0;
-	if(addSalt){
-		randomSeed(analogRead(A5));
-		salt = random(interval/100);
-		Serial.print("salt: ");
-		Serial.println(salt);
-	}
-	Timer_t timer;
-	timer.lastTime = millis();
-	timer.interval = interval + salt;
-	timer.repetitions = -1;
-	timer.running = true;
-	timer.callback = callback;
-	addTimer(timer);
-
+void Alarm::oneTimeTimer(long interval, void (*callback)(void *), void *data){
+#ifdef DEBUG
+	Serial.println("One time timer scheduled.");
+#endif	
+	repeatTimer(interval, 1,  callback, data);
 };
 
 void Alarm::checkTimers(){
 	int i;
 	long now = millis();
-	for (i = 0; i < alocatedTimers; ++i)
+	for (i = 0; i < Alarm::alocatedTimers; ++i)
 	{
-		if(timers[i].running == true){
-			if(timers[i].lastTime + timers[i].interval < now){
+		if(Alarm::timers[i].running == true){
+			if(Alarm::timers[i].lastTime + Alarm::timers[i].interval < now){
 #ifdef DEBUG
 				Serial.print("Last Time: ");
-				Serial.println(timers[i].lastTime);
+				Serial.println(Alarm::timers[i].lastTime);
 				Serial.print("Last Time + interval: ");
-				Serial.println(timers[i].lastTime + timers[i].interval);
+				Serial.println(Alarm::timers[i].lastTime + Alarm::timers[i].interval);
 				Serial.print("Now: ");
 				Serial.println(now);
 #endif
-				timers[i].lastTime = now;
-				if(timers[i].repetitions!=-1){
-					timers[i].repetitions--;
-					if(timers[i].repetitions <= 0){
-						timers[i].running = false;
-	#ifdef DEBUG
+				Alarm::timers[i].lastTime = now;
+				if(Alarm::timers[i].repetitions!=-1){
+					Alarm::timers[i].repetitions--;
+					if(Alarm::timers[i].repetitions <= 0){
+						Alarm::timers[i].running = false;
+#ifdef DEBUG
 						Serial.println("Timer stoped");
-	#endif
+#endif
 					}
 				}
-				timers[i].callback();
+#ifdef DEBUG
+				Serial.println("Calling callback.");
+#endif	
+				Alarm::timers[i].callback(Alarm::timers[i].data);
+#ifdef DEBUG
+				Serial.println("Callback called.");
+#endif	
 			}
 		}
 	}
 }
 
-void Alarm::addTimer(Timer_t newTimer){
-	timers[alocatedTimers++] = newTimer;
+void Alarm::addTimer(void *newTimer){
+	Timer_t *t = (Timer_t*) newTimer;
+	t->lastTime = millis();
+#ifdef DEBUG
+	Serial.print("lastTime: ");
+	Serial.print(t->lastTime);
+	Serial.print(" interval: ");
+	Serial.print(t->interval);
+	Serial.print(" repetitions: ");
+	Serial.print(t->repetitions);
+	Serial.print(" running: ");
+	Serial.println(t->running);
+#endif
+	
+	Alarm::timers[Alarm::alocatedTimers++] = *t;
+#ifdef DEBUG
+	Serial.println("Timer added!");
+#endif
 }
--- Arduino/Code/SensorsController/Alarm.h
+++ Arduino/Code/SensorsController/Alarm.h
@@ -3,11 +3,10 @@
 
 #include "common.h"
 
-#define AIR_TEMPERATURE_INTERVAL 10*60*1000
-#define AIR_HUMIDITY_INTERVAL 10*60*1000
-#define FISHTANK_WATER_TEMPERATURE_INTERVAL 10*60*1000
-#define RAIN_SENSOR_INTERVAL 10*60*1000
-
+#define AIR_TEMPERATURE_INTERVAL 5*1000//10*60*1000
+#define AIR_HUMIDITY_INTERVAL 5*1000//10*60*1000
+#define FISHTANK_WATER_TEMPERATURE_INTERVAL 5*1000/10*60*1000
+#define RAIN_SENSOR_INTERVAL 5*1000//10*60*1000
 
 #define MAX_TIMERS 10
 typedef struct Timer { 
@@ -15,20 +14,22 @@ typedef struct Timer {
     long interval;
     int repetitions;
     bool running;
-    void (*callback)();
+    void (*callback)(void *);
+    void *data;
 } Timer_t;
 
 class Alarm
 {
-	Timer_t timers[MAX_TIMERS];
-	int alocatedTimers = 0;
-	void addTimer(Timer_t newTimer);
+
+	static Timer_t timers[MAX_TIMERS];
+	static int alocatedTimers;
+	static void addTimer(void *newTimer);
 
 public:
 	Alarm();
-	void repeatTimer(long interval, int numberOfRepetitions, void (*callback)(void), bool addSalt = false);
-	void infiniteTimer(long interval, void (*callback)(void), bool addSalt = false);
-	void oneTimeTimer(long interval, void (*callback)(void));
+	void repeatTimer(long interval, int numberOfRepetitions, void (*callback)(void *), void *data = NULL, bool addStartDelay = false, long startDelay = -1);
+	void infiniteTimer(long interval, void (*callback)(void *), void *data = NULL, bool addStartDelay = false, long startDelay = -1);
+	void oneTimeTimer(long interval, void (*callback)(void *), void *data = NULL);
 
 	void checkTimers();
 	long lastTime;
